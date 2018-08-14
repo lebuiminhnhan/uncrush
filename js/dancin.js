42c8d298e72c95dbf792f190032d1be2f79643a6
lebuiminhnhan <37339269+lebuiminhnhan@users.noreply.github.com>
2018-08-14 12:27:30 +0700

    Update dancin.js

--- js/dancin.js
+++ js/dancin.js
@@ -5,7 +5,7 @@ function countdown(element, minutes, seconds) {
         var el = document.getElementById(element);
         // if the time is 0 then end the counter
         if (time <= 0) {
-            var text = "<p id="demo1" class="time"></p>";
+            var text = "<p id='demo1' class='time'></p>";
             el.innerHTML = text;
 
             setTimeout(function () {
